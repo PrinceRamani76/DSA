ba59e273d2882e3c976a6c8b297598638ac4fa45
PrinceRamani76 <temptemptemptemp99999@gmail.com>
2026-09-25 08:42:15 +0530

    Add files via upload

---
+++ UNIT-1/dimensional-array.py
@@ -0,0 +1,21 @@
+from numpy import *;
+
+a= array([1,2,3])
+
+print(a)
+
+a=array([[1,2,3], [4,5,6], [7,8,9]])
+print(a)
+
+a=array([
+    [[1,2,3], [4,5,6], [7,8,9]], 
+   [[10,11,12], [13,14,15], [16,17,18]]
+])
+print(a)
+
+print("ndim = ",a.ndim)
+print("shape = ", a.shape)
+print("size = ",a.size)
+print("itemsize = ",a.itemsize)
+print("dtype = ",a.dtype)
+print("nbytes = ",a.nbytes)
